bb3744fef2848066e8df1ce02cd82245fff89365
Usha Roshini Kalla <111605331+UshaKalla@users.noreply.github.com>
2022-08-19 19:24:11 -0700

    Create Class.java

---
+++ Class.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    
+    public void add(double x, double y)
+    {
+        double result = x + y; 
+        System.out.println(result);
+        
+    }
+    
+    public void subtract(double x, double y)
+    {
+        double result = x - y; 
+        System.out.println(result);
+        
+    }
+    public void multiply(double x, double y)
+    {
+        double result = x*y;
+        System.out.println(result);
+    }
+    
+    public void divide(double x, double y)
+    {
+        double result = x/y;
+        System.out.println(result);
+    }
+    
+
+}
